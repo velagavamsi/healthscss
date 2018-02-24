f99c66ed513b9ef8cc66f77fc15ee3a17390c600
velagavamsi <35959033+velagavamsi@users.noreply.github.com>
2018-02-24 13:23:15 +0530

    prev next in index component

--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -6,8 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./index.component.scss']
 })
 export class IndexComponent implements OnInit {
+  selectIndex: number = 0;
+  nearest: Array<any> = [
+    {span:'Nearest', h1:'Hospitals'},
+    {span:'Nearest', h1:'Blood Banks'},
+    {span:'Nearest', h1:'Pharmacies'}
+  ];
 
   constructor() { }
+  _next(){
+     ++this.selectIndex;
+     if(this.nearest.length == this.selectIndex)  
+      this.selectIndex = 0;
+  }
+  _prev(){
+    --this.selectIndex;
+    if(this.selectIndex < 0)
+    this.selectIndex = this.nearest.length - 1;
+  }
 
   ngOnInit() {
   }
